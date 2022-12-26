5e7fb839f59e2ef1b36874ea61755fc0a52b13a3
webots <webots@yeah.net>
2022-12-27 02:07:53 +0800

    Signed-off-by: webots <webots@yeah.net>

--- test1/图网络模型.py
+++ test1/图网络模型.py
@@ -59,52 +59,102 @@ yt=torch.Tensor(np.random.rand(3,1))
 
 # 12*4 
 
-def gcn(AT,XT,ET,outSize):
+def gcn(AT,XT,ET,W1,W2,b1):
     D12=degAT(AT)
-    W1=torch.Tensor(np.random.rand(XT.shape[1],outSize))
-    W2=torch.Tensor(np.random.rand(ET.shape[1],outSize))
-    b1=torch.Tensor(np.random.rand(AT.shape[1],1))
+    # W1=torch.Tensor(np.random.rand(XT.shape[1],outSize1))
+    # W2=torch.Tensor(np.random.rand(ET.shape[1],outSize1))
+    # b1=torch.Tensor(np.random.rand(AT.shape[1],1))
+    
     AX=D12.mm(AT).mm(D12).mm(XT).mm(W1)
     WX=torch.sum(ET.mm(W2))
     y=AX+WX+b1
     return y
 
 
-H1=gcn(AT,XT,ET,7)
+#H1=gcn(AT,XT,ET,7)
     
-def corssNN(XT,ET,YT):
+def corssNN(XT,ET,YT,W3,W4,b2):
     # XT 5*3   W3       YT 6*1
     # 5*3 3*6  6*1 5*1
-    W3=torch.Tensor(np.random.rand(XT.shape[1],YT.shape[0]))
+    # W3=torch.Tensor(np.random.rand(XT.shape[1],YT.shape[0]))
+    # W4=torch.Tensor(np.random.rand(ET.shape[1],YT.shape[0]))
+    # b2=torch.Tensor(np.random.rand(XT.shape[0],1))
+    
     
     xyT=XT.mm(W3).mm(YT)
     # ET 12*4 W4        YT 6*1
     # 12*4 4*6 6*1  12*1
-    W4=torch.Tensor(np.random.rand(ET.shape[1],YT.shape[0]))
+    
     eyT=torch.sum(ET.mm(W4).mm(YT))
     
-    b2=torch.Tensor(np.random.rand(XT.shape[0],1))
+   
     
     y=xyT+eyT+b2
     return y
     
     
-H2=corssNN(XT,ET,YT)
-print(H1.shape)
-print(H2.shape)
-    
+#H2=corssNN(XT,ET,YT)
 
-def mutH1H2(H1,H2,outSize):
-    W5=torch.Tensor(np.random.rand(H1.shape[1],H2.shape[0]))
-    W6=torch.Tensor(np.random.rand(H2.shape[1],outSize))
+
+def mutH1H2(H1,H2,W5,W6):
+    # W5=torch.Tensor(np.random.rand(H1.shape[1],H2.shape[0]))
+    # W6=torch.Tensor(np.random.rand(H2.shape[1],outSize2))
+    
     H12=H1.mm(W5).mm(H2).mm(W6)
     return H12
 
-H12=mutH1H2(H1,H2,12)
-
-Linear=nn.Linear(12,3)
-out=Linear(H12)
-print(H12.shape,out.shape)
+#H12=mutH1H2(H1,H2)
 
 
 
+#%%
+
+class Quadratic(nn.Module):
+    def init(self, a, b, c):
+        super(Quadratic, self).init()
+        self.a = nn.Parameter(torch.tensor(a))
+        self.b = nn.Parameter(torch.tensor(b))
+        self.c = nn.Parameter(torch.tensor(c))
+    
+    def forward(self, x):
+        
+        return self.a * x**2 + self.b * x + self.c
+
+#%% 
+class Model(nn.Module):
+    def init(self,AT,XT,ET,YT,outSize1,outSize2):
+        """
+        W1=torch.Tensor(np.random.rand(XT.shape[1],outSize1))
+        W2=torch.Tensor(np.random.rand(ET.shape[1],outSize1))
+        b1=torch.Tensor(np.random.rand(AT.shape[1],1))
+        W3=torch.Tensor(np.random.rand(XT.shape[1],YT.shape[0]))
+        W4=torch.Tensor(np.random.rand(ET.shape[1],YT.shape[0]))
+        b2=torch.Tensor(np.random.rand(XT.shape[0],1))
+        W5=torch.Tensor(np.random.rand(H1.shape[1],H2.shape[0]))
+        W6=torch.Tensor(np.random.rand(H2.shape[1],outSize2))
+    
+        """
+        
+        self.W1=torch.Tensor(np.random.rand(XT.shape[1],outSize1))
+        self.W2=torch.Tensor(np.random.rand(ET.shape[1],outSize1))
+        self.W3=torch.Tensor(np.random.rand(XT.shape[1],YT.shape[0]))
+        self.W4=torch.Tensor(np.random.rand(ET.shape[1],YT.shape[0]))
+        self.b1=torch.Tensor(np.random.rand(AT.shape[1],1))
+        self.b2=torch.Tensor(np.random.rand(XT.shape[0],1))
+        
+        H1= gcn(AT,XT,ET,self.W1,self.W2,self.b1)
+        H2= corssNN(XT,ET,YT,self.W3,self.W4,self.b2)
+        
+            
+        self.W5=torch.Tensor(np.random.rand(H1.shape[1],H2.shape[0]))
+        self.W6=torch.Tensor(np.random.rand(H2.shape[1],outSize2))
+        
+        self.relu=nn.relu()
+        self.sigmoid=nn.sigmoid()
+        
+    def forward(self,AT,XT,ET,YT):
+        
+        pass
+    
+    
+    
