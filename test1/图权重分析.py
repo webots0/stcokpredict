e62d965ffb07c2f48c4612c09c31e5f5ea721894
webots <webots@yeah.net>
2022-12-28 17:50:22 +0800

    Signed-off-by: webots <webots@yeah.net>

--- test1/图权重分析.py
+++ test1/图权重分析.py
@@ -44,21 +44,36 @@ def plotG(adj_matrix):
 
 
 #%%
+from matplotlib.animation import FuncAnimation
+
+# 定义画图函数
+
+# 定义图像数据
+
+
+# 动态展示图像
+
 import os
 def grafW(matrix):
+    amx=matrix.max()
+    amin=matrix.min()
+    c=0.5
+    amx*0.5
     matrix = torch.where(matrix > 0, torch.tensor(1.), torch.tensor(0.)) # 将矩阵里面的大于0的变成1，小于等于0的变成0
 
+    
     for i in range(matrix.size(0)):
         matrix[i, i] = 0. # 将矩阵对角线赋值为1
     return matrix
 
 L=os.listdir('WeightData')
 idx=0
+data=[]
 for i in L:
 
 
     a1=torch.load('WeightData/'+i)
-    mat=grafW(a1['W4']-torch.mean(a1['W4'])).numpy()
+    #mat=grafW(a1['W4']-torch.mean(a1['W4'])).numpy()
     if idx==0:
         R1=a1['W4']
     else:
@@ -69,13 +84,34 @@ for i in L:
         print(a0)
         
     #print(a1['W4'])
-    plotG(mat)
+   # plotG(mat)
     idx=idx+1
-    R1=R2
-  
-    
+    r=a1['W6']
+    data.append(r)
+   
+   
+
 
 #%% a4=grafW(aa).numpy()
 
     
+#fig, ax = plt.subplots()
+def update0(num):
+    plt.imshow(data[num])
+    #plt.show()
+ani = FuncAnimation(plt.gcf(), update0, frames=len(data), interval=1)
+plt.show()
+  
+#%%
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# 定义画图函数
+def update(i):
+    plt.plot(i, i ** 2, 'o')
+
+# 创建动画
+ani = animation.FuncAnimation(plt.gcf(), update, frames=range(100), interval=500)
+plt.show()
+    
