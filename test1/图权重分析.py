6f882c37bf92a6dbf65b91787b95550548f70994
webots <webots@yeah.net>
2022-12-28 18:00:53 +0800

    Signed-off-by: webots <webots@yeah.net>

--- test1/图权重分析.py
+++ test1/图权重分析.py
@@ -97,9 +97,9 @@ for i in L:
     
 #fig, ax = plt.subplots()
 def update0(num):
-    plt.imshow(data[num])
+    plt.imshow(torch.rand(13,13).mm(data[num]))
     #plt.show()
-ani = FuncAnimation(plt.gcf(), update0, frames=len(data), interval=1)
+ani = FuncAnimation(plt.gcf(), update0, frames=len(data), interval=500)
 plt.show()
   
 #%%
@@ -114,4 +114,52 @@ def update(i):
 # 创建动画
 ani = animation.FuncAnimation(plt.gcf(), update, frames=range(100), interval=500)
 plt.show()
-    
+#%%
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+fig = plt.figure(figsize=(10, 5))  # 创建图
+plt.rcParams["font.family"] = "FangSong"  # 支持中文显示
+plt.ylim(-12, 12)  # Y轴取值范围
+plt.yticks([-12 + 2 * i for i in range(13)], [-12 + 2 * i for i in range(13)])  # Y轴刻度
+plt.xlim(0, 2 * np.pi)  # X轴取值范围
+plt.xticks([0.5 * i for i in range(14)], [0.5 * i for i in range(14)])  # X轴刻度
+plt.title("函数 y = 10 * sin(x) 在[0,2Π]区间的曲线")   # 标题
+plt.xlabel("X轴")  # X轴标签
+plt.ylabel("Y轴")  # Y轴标签
+x, y = [], []  # 用于保存绘图数据，最开始时什么都没有，默认为空
+
+
+def update(n):  # 更新函数
+    x.append(n)  # 添加X轴坐标
+    y.append(10 * np.sin(n))  # 添加Y轴坐标
+    plt.plot(x, y, "r--")  # 绘制折线图
+
+
+ani = FuncAnimation(fig, update, frames=np.arange(0, 2 * np.pi, 0.1), interval=50, blit=False, repeat=False)  # 创建动画效果
+plt.show()  # 显示图片 作者：手把手教你学编程 https://www.bilibili.com/read/cv13169116 出处：bilibili    
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+fig, ax = plt.subplots()  # 创建画布和绘图区
+ax.set_axis_off()  # 不显示坐标轴
+x = np.arange(0, 2 * np.pi, 0.01)  # 生成X轴坐标序列
+line1, = ax.plot(x, np.sin(x))  # 获取折线图对象，逗号不可少，如果没有逗号，得到的是元组
+line2, = ax.plot(x, np.cos(x))  # 获取折线图对象，逗号不可少
+
+
+def update(n):  # 动态更新函数
+    line1.set_ydata(np.sin(x + n / 10.0))  # 改变线条y的坐标值
+    line2.set_ydata(np.cos(x + n / 10.0))  # 改变线条y的坐标值
+
+
+ani = FuncAnimation(fig, update, frames=10, interval=50, blit=False, repeat=False)  # 创建动画效果
+plt.show()  # 显示图 作者：手把手教你学编程 https://www.bilibili.com/read/cv13169116 出处：bilibili
